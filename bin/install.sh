30d4e6165203e1932a902a53eee8a3b931842d09
jibaigao <jibaigao@jj.jj>
2020-05-31 10:12:35 -0200

    update

--- bin/install.sh
+++ bin/install.sh
@@ -1,3 +1,7 @@
+hline="-------------------------------------------------------------"
+
+echo $hline
+
 if ! type "zsh" > /dev/null;then
   sudo apt install zsh
   sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -5,18 +9,18 @@ else
   zsh --version
 fi
 
-echo "----------"
+echo $hline
 
 
 if ! type "nodejs" > /dev/null;then
   curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
   sudo apt install nodejs
 else
-  echo "nodejs version: "
-  nodejs --version
+  nodeversion=`nodejs --version`
+  echo "nodejs: $nodeversion"
 fi
 
-echo "----------"
+echo $hline
 
 if ! type "vim" > /dev/null;then
   sudo add-apt-repository ppa:jonathonf/vim
@@ -25,5 +29,27 @@ if ! type "vim" > /dev/null;then
 else
   vim --version | grep "VIM - "
 fi
+
+echo $hline
+
+sshdconfig=/etc/ssh/sshd_config
+if ! [ -f "$sshdconfig" ] > /dev/null;then
+  sudo apt install openssh-server
+  sudo systemctl start ssh
+else
+  echo "$sshdconfig exists."
+fi
+
+echo $hline
+
+if ! type "chromium-browser" > /dev/null;then
+  sudo apt install chromium-browser
+else
+  chromium-browser --version
+fi
+
+echo $hline
+
+
 
 exit 0
--- setup-linuxmint.md
+++ setup-linuxmint.md
@@ -1,14 +1,15 @@
 # 設定 LinuxMint 做為 JavaScript 工作站
 
-## 安裝嘸蝦米
+## 嘸蝦米
 sudo apt install fcitx fcitx-m17n
 sudo apt install fcitx-table-boshiamy
 然後重新登入應該就可以使用了。
 如果有問題，可以打開 fcitx 設定工具看看是那裡出了問題。
 
-## 安裝 git
+## git
+sudo apt install git
 
-## 安裝 zsh 與圖示字型
+## zsh 與圖示字型
 sudo apt install zsh
 安裝oh-my-zsh：
 ```
@@ -18,12 +19,12 @@ sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools
 
 在 .zshrc 檔內將主題更換為 agnoster
 
-## 安裝 nodejs
+## nodejs
 因為官方軟體庫目前的版本只到 8.10，所以要安裝最新的版本可以上 nodesource 的 github 下載：
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt install nodejs
 
-## 安裝 vim 並設定 vimrc
+## vim 與 vimrc
 sudo apt install vim
 但是官方軟體庫目前還是 vim 8.0，想要安裝 8.2 就要自已加入第三方軟體庫：
 sudo add-apt-repository ppa:jonathonf/vim
@@ -34,3 +35,15 @@ sudo apt install vim
 
 上 git 下載設定好的 vimrc
 
+## openssh-server
+sudo apt install openssh-server
+sudo systemctl start ssh
+
+## chromium
+sudo apt install chromium-browser
+
+
+## JavaScript 環境設定
+
+### eslint 安裝
+sudo npm install --global eslint
